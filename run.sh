919f804ebb075e0ef0346d134a6a92bd09de906a
V0vaG <its_a_vio@hotmail.com>
2024-10-08 16:04:38 +0300

    auto_push

--- run.sh
+++ run.sh
@@ -25,6 +25,9 @@ sudo rm /home/ubuntu/run.sh
 sudo rm /home/ubuntu/admin.conf
 sudo rm /home/ubuntu/solitare.yaml
 
+# sudo rm /home/ubuntu/docker-compose.yml
+# cd /home/ubuntu && docker-compose down
+
 sudo cat << EOF > /home/ubuntu/docker-compose.yml
 version: "3"
 
